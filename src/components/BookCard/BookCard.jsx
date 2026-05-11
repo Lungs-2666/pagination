33e77a56a109c395e79abb1114de7598f1536c78
FocUUsMe <timoxxastolarov@gmail.com>
2026-05-11 17:45:33 +0300

    fc

---
+++ src/components/BookCard/BookCard.jsx
@@ -0,0 +1,11 @@
+    import './BookCard.css';
+    
+    const BookCard = () => {
+        return (
+            <div className="book_card">
+                
+            </div>
+        )
+    }
+
+    export default BookCard;
